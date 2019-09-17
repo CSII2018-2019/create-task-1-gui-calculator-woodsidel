9380b07b7b4e900ef6f1c9998421575e02743780
woodsidel <woodsidel@LR064WA7-1718.ellis.int>
2019-09-16 20:11:10 -0400

    Final commit

--- src/CalculatorGUI.java
+++ src/CalculatorGUI.java
@@ -17,11 +17,11 @@ import javax.swing.JTextField;
 import javax.swing.UIManager;
 
 public class CalculatorGUI extends JFrame {
-	
+
 	public JTextField in1;
 	public JTextField in2;
 	public JLabel answer;
-	
+
 	public CalculatorGUI() {
         initGUI();
 
@@ -31,50 +31,48 @@ public class CalculatorGUI extends JFrame {
         setSize(200, 200);
         setResizable(true);
         setLocationRelativeTo(null);
-        
-        
+
+
         pack();
     }
 
 	private void initGUI() {
-		
-		
+
+
 		JLabel titleLabel = new JLabel("Calculator");
         titleLabel.setHorizontalAlignment(JLabel.CENTER);
         Font titleFont = new Font(Font.SERIF, Font.PLAIN, 32);
         titleLabel.setFont(titleFont);
         add(titleLabel, BorderLayout.PAGE_START);
-        
-        
+
+
         //number panel set-up
         JPanel numberInPanel = new JPanel();
         numberInPanel.setLayout(new GridLayout(2, 2));
         numberInPanel.setPreferredSize(new Dimension(250, 75));
         numberInPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
         add(numberInPanel, BorderLayout.LINE_START);
-        
+
         JLabel num1 = new JLabel("First Number: ");
         numberInPanel.add(num1);
-             
+
         in1 = new JTextField(10);
         numberInPanel.add(in1);
-        
+
         JLabel num2 = new JLabel("Second Number: ");
         numberInPanel.add(num2);
-        
+
         in2 = new JTextField(10);
         numberInPanel.add(in2);
-        
-    
-        
+
         //set-up button panel
         JPanel buttonPanel = new JPanel();
         buttonPanel.setLayout(new GridLayout(2, 2));
         buttonPanel.setPreferredSize(new Dimension (300, 50));
         buttonPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
         add(buttonPanel, BorderLayout.LINE_END);
-        
-        
+
+
         //average button set-up
         JButton averageBtn = new JButton("Average");
         averageBtn.addActionListener(new ActionListener() {
@@ -84,8 +82,8 @@ public class CalculatorGUI extends JFrame {
             }
         });
         buttonPanel.add(averageBtn);
-        
-        
+
+
         //factorial button set-up
         JButton factorialBtn = new JButton("Factorial");
         factorialBtn.addActionListener(new ActionListener() {
@@ -95,8 +93,8 @@ public class CalculatorGUI extends JFrame {
             }
         });
         buttonPanel.add(factorialBtn);
-        
-        
+
+
         //higher number button set-up
         JButton higherBtn = new JButton("Higher");
         higherBtn.addActionListener(new ActionListener() {
@@ -106,9 +104,9 @@ public class CalculatorGUI extends JFrame {
             }
         });
         buttonPanel.add(higherBtn);
-        
-        
-        
+
+
+
       //percent change button set-up
         JButton perChangeBtn = new JButton("Percent Change");
         perChangeBtn.addActionListener(new ActionListener() {
@@ -118,51 +116,73 @@ public class CalculatorGUI extends JFrame {
             }
         });
         buttonPanel.add(perChangeBtn);
-        
-        
-        
+
+
+
         answer = new JLabel("Answer: ");
         answer.setPreferredSize(new Dimension (500, 100));
         Font answerFont = new Font(Font.SERIF, Font.PLAIN, 20);
         answer.setFont(answerFont);
         add(answer, BorderLayout.PAGE_END);
-       
-        
+
+
 	}
 
 	private void calcHigher() {
-		// TODO Auto-generated method stub
-		
+		double x = Double.parseDouble(in1.getText());
+		double y = Double.parseDouble(in2.getText());
+		double high = 0;
+		if(x > y) {
+			answer.setText("Higher Number " + x);
+		}
+		else if (x == y) {
+			answer.setText("The numbers are equal");
+		}
+		else {
+			answer.setText("Higher Number " + y);
+		}
+
 	}
 
 	private void calcPerChange() {
-		// TODO Auto-generated method stub
-		
+		double x = Double.parseDouble(in1.getText());
+		double y = Double.parseDouble(in2.getText());
+		double difference = 0;
+		if(x > y) {
+			difference = x-y;
+		}
+		else {
+			difference = y-x;
+		}
+		double perChange = (difference/x)*100;
+		answer.setText("Percent Change: "+perChange);
+
 	}
 
 	private void calcFactorial() {
-		String integer = in1.getText();
-		int factInt = Integer.parseInt(integer);
-		while(factInt != 1) {
-			factInt = factInt*(factInt-1);
-			factInt--;
+		//Source bc I needed help: https://www.geeksforgeeks.org/java-program-for-factorial-of-a-number/
+		double x = Double.parseDouble(in1.getText());
+		double y = Double.parseDouble(in2.getText());
+		double num = x+y;
+		double fact = 1;
+		for(int i = 2; i <= num; i++) {
+			fact *= i;
 		}
-		answer.setText("The factorial is: " + factInt);
+		answer.setText("Factorial is: " + fact);
+		
 			
 	}
-		
-	
 
 	private void calcAvg() {
 		double x = Double.parseDouble(in1.getText());
 		double y = Double.parseDouble(in2.getText());
-		
+
 		double avg = (x + y)/2;
-		
+
 		answer.setText("Average is: " + avg);
-		
-		
-		
+
+
+
 	}
 
 	public static void main(String[] args) {
